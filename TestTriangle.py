b06255d47cc6748cc394739feee66cc6299ca9e5
evan ciok <eciok@stevens.edu>
2023-09-27 17:18:58 -0400

    updated Triangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -7,8 +7,17 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 @author: rk
 """
 
-import unittest
+from datetime import datetime
+def my_brand(assignment):
+  datetime_obj = datetime.now()
+  datetime_string = datetime_obj.strftime("%d/%m/%Y %H:%M:%S")
+  print("=*=*=*= Evan Ciok =*=*=*=")
+  print("=*=*=*= 2023F SSW 567-A =*=*=*=")
+  print("=*=*=*= " + assignment + " =*=*=*=")
+  print("=*=*=*= " + datetime_string + " =*=*=*=")
+assignment_name = "HW 02a"
 
+import unittest
 from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
